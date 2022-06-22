db943d670c39ae3567d7beb45ca41d9157edf22d
siemeris <siemeris@gmail.com>
2022-06-22 17:17:00 +0000

    uploading my work

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,19 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+//import Home from "./component/home.jsx";
+const SecondsCounter = (props) => {
+    return (<div className="container-fluid">
+        <div className="contador display-2 d-flex bg-dark">
+            <div className="text-white">
+        <i className="fa-regular fa fa-clock"></i></div>
+        <div className="text-white">0</div>
+        <div className="text-white">0</div>
+        <div className="text-white">0</div>
+        <div className="text-white">0</div>
+            </div> 
+    </div>)
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SecondsCounter  />, document.querySelector("#app"));
